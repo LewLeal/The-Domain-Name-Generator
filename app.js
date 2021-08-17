68a50685b78b5df927dfc39232d29ccf02f02ff5
Lewis Leal <lewleal2012@gmail.com>
2021-08-17 15:10:54 +0000

    Generator domain JS

---
+++ app.js
@@ -0,0 +1,18 @@
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let arreglo = [];
+
+
+  for (let a = 0; a < pronoun.length; a++) {
+    for (let b = 0; b < adj.length; b++) {
+      for (let c = 0; c < noun.length; c++) {
+        console.log(pronoun[a] +  adj[b] + noun[c] + ".com");
+       
+        
+      }
+    }
+  }
+
+
+
